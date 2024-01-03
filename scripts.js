a73aaba0a0d9350267922f29291308bff491fbae
davialexandreh <d19alexandre@hotmail.com>
2024-01-03 08:46:47 -0300

    Update na lógica do JS

--- scripts.js
+++ scripts.js
@@ -3,8 +3,12 @@ function generateNumber() {
 
     const entre = Math.ceil(document.querySelector('#input-entre').value)
     const e = Math.floor(document.querySelector('#input-e').value)
-    const result = Math.floor(Math.random() * (entre - e + 1)) + e;
 
-    document.getElementById('button-sortear').textContent = `Resultado do sorteio: ${result}`;
+    if (entre >= e) {
+        alert('O valor no campo "entre" tem que ser menor que o valor no campo "e"! \nPor favor, atualize a página.')
+    } else {
+        const result = Math.floor(Math.random() * (entre - e + 1)) + e;
+        document.getElementById('button-sortear').textContent = `Resultado do sorteio: ${result}`;
+    }
 }
 
